9a6ba3e9898f501fdcf86d2bc8d65653c6af4654
csabasallai <csabasallai@csabasallai-MacBook-Pro.local>
2020-11-24 12:00:46 +0100

    1

--- battleship.py
+++ battleship.py
@@ -5,19 +5,16 @@ import os
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
-def print_board():
-    first_input(board=[])
+def init_board():
+    board = []
     for i in range(0,5):
         board.append(["o"] * 5)
     print(" ", " ".join("12345"))
     for letter, row in zip('ABCDE', board):
         print(letter, " ".join(row))
-print_board()
 
 
 def first_input(board):
-    board = []
     temp_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
     while True:
         position = input("Provide a position:")
@@ -43,7 +40,7 @@ def first_input(board):
         if position not in temp_dict and try_ag == "n":
                 exit()
 
-first_input()
+
 
 def second_input():
     columns = ["1","2","3","4","5"]
@@ -52,3 +49,6 @@ def second_input():
         if col not in columns:
             print("That's not quite right, please use valid inputs")
         
+if __name__ == "__main__":
+    init_board()
+    first_input(board)
