b9923b5a84f323c3ef69102c3bfec89efe36c292
csabasallai <csabasallai@csabasallai-MacBook-Pro.local>
2020-11-24 11:49:45 +0100

    under developmen

--- battleship.py
+++ battleship.py
@@ -9,41 +9,38 @@ def clear_terminal():
 def print_board():
     board = []
     for i in range(0,5):
-        board.append(["O"] * 5)
+        board.append(["o"] * 5)
     print(" ", " ".join("12345"))
     for letter, row in zip('ABCDE', board):
         print(letter, " ".join(row))
+print_board()
 
 
 def first_input():
     board = []
-    for i in range(0,5):
-        c_board = board.append(["O"] * 5)
-    print(" ", " ".join("12345"))
-    for letter, row in zip('ABCDE', board):
-        print(letter, " ".join(row))
-    temp_dict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+    temp_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
     while True:
-        position = input("type a row and column:")
-        row_for_retry = position
+        position = input("Provide a position:")
+        if position == "quit":
+            print("Goodbye!")
+            exit()
         try:
             if position[0].lower() in ["a", "b", "c", "d", "e"] and int(position[1]) in [1, 2, 3, 4, 5]:
                 row = temp_dict.get(position[0].lower())
                 col = int(position[1]) - 1
-            if c_board[row][col] == "X":
-                c_board[row][col] = "X"
-            else:
-                print("That position isn't empty!")
+                if board[row][col] == "o":
+                    board[row][col] = "x"
+                else:
+                    print("That position isn't empty!")
         except Exception:
-            print("bamm")  
-            break
-            if position not in temp_dict:
-                print("That's not quite right , please use valid inputs")
-                try_ag = input("Do you wanna try again? y/n : ")
-            if position not in temp_dict and try_ag == "y":
-                print("The game now restarting!")
-                row_for_retry  
-            if position not in temp_dict and try_ag == "n":
+            print("bamm")
+        break
+        if position not in temp_dict:
+            print("That's not quite right , please use valid inputs")
+            try_ag = input("Do you wanna try again? y/n : ")
+        if position not in temp_dict and try_ag == "y":
+            print("The game now restarting!")  
+        if position not in temp_dict and try_ag == "n":
                 exit()
 
 first_input()
