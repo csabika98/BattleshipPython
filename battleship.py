5db7b78c233d09d8b69dbff22eb44b7bba80bfc0
necro-1337 <papaimate166@gmail.com>
2020-11-24 10:33:01 +0100

    Added ship_placement(), minor changes

--- battleship.py
+++ battleship.py
@@ -1,25 +1,43 @@
 import random
 import os
 
+board = []
+rows = ["a","A","b","B","c","C","e","E"]
+columns = ["1","2","3","4","5"]
+
 
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-rows = ["a","A","b","B","c","C","e","E"]
-columns = ["1","2","3","4","5"]
+def init_board():
+    for i in range(5):
+        board.append(["o"] * 5)
+
+
+def ship_placement(board):
+    ship_row = input("Enter the ship's position (row): ")
+    ship_col = input("Enter the ship's position (col): ")
+    ship_position = [ship_row, ship_col]
+    for i in board:
+        if i == ship_position[0]:
+            for e in i:
+                if e == ship_position[1]:
+                    e = "x"
+
+
 def print_board():
-    board = []
-    for i in range(0,5):
-        board.append(["O"] * 5)
     print(" ", " ".join("12345"))
     for letter, row in zip('ABCDE', board):
         print(letter, " ".join(row))
 
 
 print_board()
+ship_placement(board)
+print_board()
 
-while True:
+
+'''while True:
     row = input("Provide a row: ")
     if row not in rows:
         print("That's not quite right , please use valid inputs")
@@ -28,5 +46,5 @@ while True:
     if col not in columns:
         print("That's not quite right, please use valid inputs")
         continue
-        
-    break
+   
+    break'''
