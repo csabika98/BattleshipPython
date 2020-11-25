72b7c975c23997bcbcd95ea993489dfad55a1076
csabasallai <csabasallai@csabasallai-MacBook-Pro.local>
2020-11-25 09:18:17 +0100

    little bugfixes

--- battleship.py
+++ battleship.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 
 board = []
 
@@ -7,40 +8,97 @@ board = []
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def get_input(): # easy get_input
+    coordinates = input("Please enter a column and row: ")
+    return coordinates
 
 def init_board():
+    board = []
     for i in range(5):
         board.append(["o"] * 5)
+    return board 
+
+def switch_player(current_player): # switching player 1 to player 2
+    if current_player == 1:
+        current_player = 2
+    else:
+        current_player = 1
+    return current_player
 
 
-def ship_placement(board):
-    temp_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
-    while True:
-        position = input("Provide position:")
-        row = temp_dict.get(position[0].lower())
-        col = int(position[1]) - 1
-        if position == "quit":
-            print("Goodbye!")
-            exit()
-        try:
-            if position[0].lower() in ["a", "b", "c", "d", "e"] and int(position[1]) in [1, 2, 3, 4, 5]:
-                board[row][col] = "x"
-            else:
-                print("That's not quite good! Please use valid input! ")
-        except Exception: 
-            print("Good Work")
-    
+def choosing_scene():
+    board_player_1 = init_board() # board of each player
+    board_player_2 = init_board()
+    mark = "" # need mark your choices , literraly saving your choices 
+    current_player = 1 # variables to be able to switch players (player1 or player2 ) NOT FINISHED UNDER DEVELOPMENT
+    app_only_run_once = True # with this variable i am able to PRINT Board  , OR Stop the screen/print board and with this i am making sure the program runs only Once
+    while app_only_run_once: # i made a while loop using my variables if TRUE the program STARTS , if FALSE It not starts anymore
+        mark = "X"  
+        if current_player == 1: # you can decide if player1 start his choosing or player2 
+            print_board(board_player_1)
+            board = board_player_1
+        else:
+            print_board(board_player_2)
+            board = board_player_2
+        coordinates = get_input() # getting the coordinates
+        if not valid_input(coordinates):  # sorting out if you have typed a valid input or not
+            continue
+        translated_coordinates = converter(coordinates) # i make a variable to my converter
+        mark_player_coordinates(board,translated_coordinates, mark)
+        
+
+        if sum(x.count('X') for x in board) == 3:    # adding your choices to the BOARD with using the converter
+            print_board(board)
+            if current_player == 2:      # its making sure that the next player come after the player 1 chosed his X-s
+                app_only_run_once = False
+            current_player = switch_player(current_player)
+            os.system("""bash -c 'read -s -n 1 -p "Press any key to continue..."'""")
+            os.system('cls||clear')
+ 
+
+def converter(coordinates):
+    coordinates_list = list(coordinates)
+    try:
+        if coordinates_list[0] == 'A' or coordinates_list[0] == 'a':
+            coordinates_list[0] = 0
+        if coordinates_list[0] == 'B' or coordinates_list[0] == 'b':
+            coordinates_list[0] = 1
+        if coordinates_list[0] == 'C' or coordinates_list[0] == 'c':
+            coordinates_list[0] = 2
+        if coordinates_list[0] == 'D' or coordinates_list[0] == 'd':
+            coordinates_list[0] = 3
+        if coordinates_list[0] == 'E' or coordinates_list[0] == 'e':
+            coordinates_list[0] = 4
+        
+        coordinates_list[1] = int(coordinates_list[1]) - 1
+        
+    except:
+        print('ERROR: Invalid input')
+    return coordinates_list
 
 
-def print_board():
+def valid_input(coordinates):  # cheking if the input is valid or not
+    coordinates_list = list(coordinates)
+    if len(coordinates_list) !=2: # if the coordinates less than 2 charachters the program gave you invalid input
+        print("That's not gonna working, please use valid input")
+        return False
+    possible_coordinates = ['a', 'b', 'c', 'd', 'e', 'A', 'B', 'C', 'D', 'E', '1', '2', '3', '4', '5'] # your possibly choices
+    if coordinates_list[0] not in possible_coordinates or coordinates_list[1] not in possible_coordinates:
+        print("Invalid Input try agian")
+        return False
+    return True
+
+def print_board(board):
     print(" ", " ".join("12345"))
     for letter, row in zip('ABCDE', board):
         print(letter, " ".join(row))
 
 
-init_board()
-print_board()
-ship_placement(board)
-print_board()
+def mark_player_coordinates(board, coordinates, mark):  # marking your choices on the board
+    board[coordinates[0]][coordinates[1]] = mark
+    return board
 
 
+
+if __name__ == "__main__":
+    choosing_scene()
