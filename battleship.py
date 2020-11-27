7ec34f9b694bfb58eba80a53aefd9facccdece52
csabasallai <csabasallai@csabasallai-MacBook-Pro.local>
2020-11-27 11:49:52 +0100

    Final Version

--- battleship.py
+++ battleship.py
@@ -1,8 +1,7 @@
 import os
 import time
 import io
-# A board is a list of rows, and each row is a list of cells with either an 'x' (a battleship)
-# or o (water)
+
 board = []
 for i in range(5):
     board.append(["o"] * 5)
@@ -15,7 +14,7 @@ for i in range(5):
 guesses_board2 = []
 for i in range(5):
     guesses_board2.append(["o"] * 5)
-# Easy way to transform the user's input to an usable input for the program
+
 letters_to_numbers = {
     'a': 0,
     'b': 1,
@@ -25,7 +24,7 @@ letters_to_numbers = {
 }
 
 
-# Ask for the player's input
+
 def ask_player_1_user_for_board_position():
     column = input("column (A to E):").lower()
     while column not in "abcde":
@@ -37,12 +36,11 @@ def ask_player_1_user_for_board_position():
         print("That row is wrong! it should be 1, 2, 3, 4 or 5")
         row = input("row (1 to 5):")
 
-    # The code calling this function will receive the values listed in the return statement below
-    # and it can assign it to variables
+   
     return int(row) - 1, letters_to_numbers[column]
 
 
-# Ask for the player's input
+
 def ask_player_2_user_for_board_position():
     column = input("column (A to E):").lower()
     while column not in "abcde":
@@ -54,8 +52,7 @@ def ask_player_2_user_for_board_position():
         print("That row is wrong! it should be 1, 2, 3, 4 or 5")
         row = input("row (1 to 5):")
 
-    # The code calling this function will receive the values listed in the return statement below
-    # and it can assign it to variables
+    
     return int(row) - 1, letters_to_numbers[column]
 
 
@@ -69,8 +66,8 @@ def ascii_art():
     ascii_text = f"\033[91m{entire_file}\033[00m"
     print(ascii_text)
 
+
 def print_board(board):
-    # Show the board, one row at a time
     print(" ", " ".join("ABCDE"))
     for letter, row in zip('12345', board):
         print(letter, " ".join(row))
@@ -82,7 +79,7 @@ def print_board_2(board2):
         print(letter, " ".join(row))
 
 
-# We place 3 battleships for player 1
+
 ascii_art()
 time.sleep(3)
 counter = 0
@@ -92,7 +89,7 @@ while counter != 3:
     row_number, column_number = ask_player_1_user_for_board_position()
 
 
-# Check that there are no repeats
+
     if board[row_number][column_number] == 'X':
         print("That spot already has a battleship in it!")
     if board[row_number][column_number] != 'X':
@@ -104,7 +101,6 @@ while counter != 3:
 
 time.sleep(1)
 clear_terminal()
-# We place 3 battleships for player 2
 counter = 0
 while counter != 3:
     print("Player 2 turn, choose 3 ship")
@@ -112,7 +108,7 @@ while counter != 3:
     row_number, column_number = ask_player_2_user_for_board_position()
 
 
-# Check that there are no repeats
+
     if board2[row_number][column_number] == 'X':
         print("That spot already has a battleship in it!")
     if board2[row_number][column_number] != 'X':
@@ -121,10 +117,9 @@ while counter != 3:
         counter += 1
 
 time.sleep(1)
-# Now clear the screen, and the other player starts guessing
 clear_terminal()
 
-# Keep playing until we have 3 right guesses
+
 guesses_p1 = 0
 guesses_p2 = 0
 winner = ""
@@ -137,7 +132,7 @@ while True:
         print("You have already guessed that place!")
         continue
 
-    # Check that there are no repeats
+    
     if board[row_number][column_number] == 'X':
         print("HIT!")
         guesses_board[row_number][column_number] = 's'
@@ -150,7 +145,6 @@ while True:
         guesses_board[row_number][column_number] = 'm'
         print("MISS!")
     print_board(guesses_board)
-# # # # Divide the 2 player's turn's code # # # #
     print("Player 2 turn")
     print("Guess a battleship location")
     row_number, column_number = ask_player_2_user_for_board_position()
@@ -158,7 +152,7 @@ while True:
         print("You have already guessed that place!")
         continue
 
-    # Check that there are no repeats
+   
     if board2[row_number][column_number] == 'X':
         print("HIT!")
         guesses_board2[row_number][column_number] = 's'
